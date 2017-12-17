124ffaf1aff4e36bfb7fb052350c0fafc53160ed
utharamadhan <utha@metamorf.co.id>
2017-12-17 15:26:24 +0700

    create a project that using centralised configuration when startup

---
+++ inventory-service/src/main/java/com/rpramadhan/commerce/inventoryservice/main/InventoryServiceMain.java
@@ -0,0 +1,16 @@
+package com.rpramadhan.commerce.inventoryservice.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rpramadhan
+ */
+@SpringBootApplication
+public class InventoryServiceMain {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(InventoryServiceMain.class, args);
+	}
+
+}
